8347d652fd763d07a04c273a5c2490f38b243223
Mario Lubenka <mario.lubenka@googlemail.com>
2020-06-11 14:14:47 +0200

    feat: Introduce markers_split setting

--- src/nginx/nginx.go
+++ src/nginx/nginx.go
@@ -1,10 +1,12 @@
 package nginx
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
 	"regexp"
+	"strings"
 )
 
 func getNginxDirectories(filename string, m Config) (string, string) {
@@ -16,24 +18,53 @@ func getNginxDirectories(filename string, m Config) (string, string) {
 	return pathAvailable, pathEnabled
 }
 
-func CreateOrUpdateServerBlock(filename string, content string, m Config, markers map[string]interface{}) (string, error) {
+func CreateOrUpdateServerBlock(filename string, content string, m Config, markers map[string]interface{}, markersSplit map[string]interface{}) (string, error) {
 	fullPathAvailable, _ := getNginxDirectories(filename, m)
-
-	// Replace markers in content
-	var re *regexp.Regexp
-	for key, value := range markers {
-		re, _ = regexp.Compile("{#\\s*" + key + "\\s*#}") // {#marker#}
-		content = re.ReplaceAllString(content, value.(string))
-		re, _ = regexp.Compile("{~\\s*" + key + "\\s*~}") // {~marker~}
-		content = re.ReplaceAllString(content, value.(string))
-		re, _ = regexp.Compile("{\\*\\s*" + key + "\\s*\\*}") // {*marker*}
-		content = re.ReplaceAllString(content, value.(string))
-	}
+	content = ReplaceMarkers(content, ProcessMarkers(markers, markersSplit))
 
 	if err := ioutil.WriteFile(fullPathAvailable, []byte(content), 0744); err != nil {
 		return "", err
 	}
 	return fullPathAvailable, nil
+}
+
+/// ReplaceMarkers replaces all markers in given content
+func ReplaceMarkers(content string, markers map[string]string) string {
+	var re *regexp.Regexp
+	for key, value := range markers {
+		quotedKey := regexp.QuoteMeta(key)
+		re, _ = regexp.Compile("{#\\s*" + quotedKey + "\\s*#}") // {#marker#}
+		content = re.ReplaceAllString(content, value)
+		re, _ = regexp.Compile("{~\\s*" + quotedKey + "\\s*~}") // {~marker~}
+		content = re.ReplaceAllString(content, value)
+		re, _ = regexp.Compile("{\\*\\s*" + quotedKey + "\\s*\\*}") // {*marker*}
+		content = re.ReplaceAllString(content, value)
+	}
+	return content
+}
+
+/// ProcessMarkers resolves array values into single string replaces
+func ProcessMarkers(markers map[string]interface{}, markers_split map[string]interface{}) map[string]string {
+	markers_split_keys := make([]string, 0, len(markers_split))
+	for k := range markers_split {
+		markers_split_keys = append(markers_split_keys, k)
+	}
+
+	output := make(map[string]string)
+	for key, value := range markers {
+		stringValue := value.(string)
+		splitChar := markers_split[key]
+		if splitChar == nil || splitChar.(string) == "" {
+			output[key] = stringValue
+			continue
+		}
+
+		// Split value by character
+		for i, slice := range strings.Split(stringValue, splitChar.(string)) {
+			output[fmt.Sprintf("%s[%d]", key, i)] = slice
+		}
+	}
+	return output
 }
 
 func RemoveNginxServerBlock(filename string, m Config) error {
